399ddad2e6d0dbeb5fc4411f255df3a190f8b299
Mariusz Dziuba <mardziuba@gmail.com>
2023-04-22 19:52:07 +0200

    commit

---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,28 @@
+///Users/mariuszdziuba/Desktop/Development/Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Mariusz Dziuba on 30/10/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
